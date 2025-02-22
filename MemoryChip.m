ea6bcf7c6d3784d9479377bee96b4a1aff998470
economyplusdev <barcodetm25@gmail.com>
2025-02-22 00:08:47 -0500

    Update MemoryChip.m

--- MemoryChip.m
+++ MemoryChip.m
@@ -5,16 +5,18 @@ classdef CPU
         banks
         bankSize
         memory
+        occupied
     end
     methods (Static)
         function main
-            clear; clc;
+            clc; clear;
             c = CPU(1024,8,4);
             for i = 1:10
                 a = c.generateAddress();
-                c = c.write(a, randi([0,255]));
+                c = c.write(a, randi([1,255]));
                 disp(['0x' dec2hex(a) ' ' num2str(c.read(a))])
             end
+            c.displayGraph();
         end
     end
     methods
@@ -24,8 +26,10 @@ classdef CPU
             obj.banks = b;
             obj.bankSize = tm/b;
             obj.memory = cell(b,1);
+            obj.occupied = cell(b,1);
             for i = 1:b
                 obj.memory{i} = zeros(obj.bankSize,1,'uint8');
+                obj.occupied{i} = false(obj.bankSize,1);
             end
         end
         function addr = generateAddress(obj)
@@ -37,11 +41,25 @@ classdef CPU
             bank = floor((addr-1)/obj.bankSize) + 1;
             offset = mod(addr-1,obj.bankSize) + 1;
             obj.memory{bank}(offset) = uint8(value);
+            obj.occupied{bank}(offset) = true;
         end
         function value = read(obj,addr)
             bank = floor((addr-1)/obj.bankSize) + 1;
             offset = mod(addr-1,obj.bankSize) + 1;
             value = obj.memory{bank}(offset);
+        end
+        function displayGraph(obj)
+            occupancyMatrix = zeros(obj.bankSize, obj.banks);
+            for i = 1:obj.banks
+                occupancyMatrix(:,i) = double(obj.occupied{i});
+            end
+            figure; imagesc(occupancyMatrix);
+            colormap([1 1 1; 0 0 0]);
+            title('Memory Occupancy');
+            xlabel('Bank');
+            ylabel('Cell Index');
+            set(gca,'XTick',1:obj.banks);
+            axis equal tight;
         end
     end
 end
